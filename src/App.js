9f43188dee64de43bb313c3845993d1557763fd2
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-12-08 16:26:25 -0600

    init

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
 const App = () => {
+  const dummyCastA = {
+    id: 1,
+    name: "Chris Razo",
+    role: "Director",
+    socials: [
+      { instagram: "razo_christian" },
+      { linkedIn: "christian-ra" },
+      { tikTok: "christianrazo09" },
+    ],
+  };
   return (
     <div className='App'>
       <p>Welcome to reelz!</p>
