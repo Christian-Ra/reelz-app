b274869032bd783bcfc059f22faef6f6a850df24
Christian Razo <37232875+Christian-Ra@users.noreply.github.com>
2023-12-11 15:49:27 -0600

    Displays castList and respective socials

--- src/components/CastMember.js
+++ src/components/CastMember.js
@@ -1,5 +1,19 @@
-const CastMember = (cast) => {
-  return <div>This is where a cast member goes</div>;
+const CastMember = ({ cast }) => {
+  console.log("cast object", cast);
+  console.log("cast name", cast.name);
+  console.log("Cast socials", cast.socials);
+  return (
+    <div>
+      <h1>Crew Member Name: {cast.name}</h1>
+      <h2>Crew Member Role: {cast.role}</h2>
+      <div>
+        <h3>Socials:</h3>
+        <h4>TikTok: {cast.socials.tikTok}</h4>
+        <h4>LinkedIn: {cast.socials.linkedIn}</h4>
+        <h4>instagram: {cast.socials.instagram}</h4>
+      </div>
+    </div>
+  );
 };
 
 export default CastMember;
